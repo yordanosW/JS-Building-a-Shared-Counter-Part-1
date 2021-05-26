3a7b410870d8a589540a9953835106867b790c85
Joshua Miles <joshua.m@hackerusa.com>
2021-05-26 10:53:48 -0500

    tweaks to dev server

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-const jsonServer = require('json-server')
+const spawn = require('child_process').spawn;
 const liveServer = require('live-server')
-const apiServer = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
 liveServer.start({
+    port: 9000,
     logLevel: 0,
     root: './public'
 })
-
-apiServer.use(middlewares)
-apiServer.use('/api', router)
-apiServer.listen(9000, () => {
-    console.log('JSON Server is running')
-})
-
+spawn('npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
