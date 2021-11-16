0b09b98d3d8bfaa63a24977869bd752a8999c849
Joshua Miles <isaacsmiles@gmail.com>
2021-11-16 09:38:49 -0600

    fixed bug for windows

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const spawn = require('child_process').spawn;
 const liveServer = require('live-server')
 const sleep = time => new Promise(resolve => setTimeout(resolve,time))
+const isWin = process.platform === 'win32'
 async function main(){
-    spawn('npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
+    spawn( isWin ? 'npx.cmd' : 'npx', ['json-server', '--watch', '--port', '9001', 'db.json' ], { stdio: 'inherit'});
     await sleep(1000) // <-- Wait one second, so that the json-server has started by the time the live-server opens.
     liveServer.start({
         port: 9000,
